b00d79652bcf3350f2b36101715113b8c1aaafd1
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2023-12-14 18:03:17 +0100

    clearer code

--- zig-server/main.zig
+++ zig-server/main.zig
@@ -1,13 +1,14 @@
 const std = @import("std");
 
 const Connection = std.net.StreamServer.Connection;
+pub const io_mode = .evented;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    var clinets = std.ArrayList(Connection).init(allocator);
-    defer clinets.deinit();
+    var room = Room{ .clinets = std.ArrayList(*Client).init(allocator) };
+    defer room.clinets.deinit();
 
     var listner = std.net.StreamServer.init(.{});
     defer listner.deinit();
@@ -16,27 +17,34 @@ pub fn main() !void {
     std.debug.print("listen at {} \n", .{listner.listen_address});
 
     while (true) {
-        const conn = try listner.accept();
-        try clinets.append(conn);
-        _ = try std.Thread.spawn(.{}, handle_clinet, .{ conn, &clinets });
+        const clinet = try allocator.create(Client);
+        clinet.* = Client{ .conn = try listner.accept(), .room = &room };
+        _ = try std.Thread.spawn(.{}, Client.handle, .{clinet});
+        try room.clinets.append(clinet);
     }
 
     return;
 }
 
-fn handle_clinet(conn: Connection, clients: *std.ArrayList(Connection)) !void {
-    std.debug.print("New Connection ip: {}\n", .{conn.address});
+const Client = struct {
+    conn: Connection,
+    room: *Room,
 
-    var buffer: [1024]u8 = undefined;
-    while (true) {
-        _ = try conn.stream.read(&buffer);
-        std.debug.print("message resieved: {s} \n", .{buffer});
-        for (clients.*.items) |client| {
-            if (client.address.getPort() != conn.address.getPort()) {
-                client.stream.writeAll(&buffer) catch |err| {
-                    std.debug.print("Could not send a message to : {}, error: {}\n", .{ client.address, err });
-                };
-            }
+    fn handle(client: *Client) !void {
+        var buffer: [1024]u8 = undefined;
+        while (true) {
+            _ = try client.conn.stream.read(&buffer);
+            try client.room.broadcast(client, buffer);
         }
     }
-}
+};
+
+const Room = struct {
+    clinets: std.ArrayList(*Client),
+    fn broadcast(self: *Room, clinet: *Client, msg: [1024]u8) !void {
+        for (self.clinets.items) |cc| {
+            if (cc == clinet) continue;
+            try cc.conn.stream.writeAll(&msg);
+        }
+    }
+};
